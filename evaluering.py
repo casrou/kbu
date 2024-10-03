82f93dc5739a15ff937007197bdeb36b3a5ae2ad
Casper Roursgaard <casper@casrou.com>
2024-10-03 20:17:13 +0200

    clean up

--- evaluering.py
+++ evaluering.py
@@ -1,13 +1,12 @@
 import json
-import requests
-from thefuzz import process
 from rapidfuzz import fuzz
 import os.path
+import random
 
 FETCH = False
 
-START_DATE = "30/09/2023"
-END_DATE = "29/09/2024"
+START_DATE = "01/01/2022"
+END_DATE = "01/01/2030"
 
 FILES = [
     "KBU Forløb - Sommeren 2022 - Runde 29.json",
@@ -15,8 +14,6 @@ FILES = [
     "KBU Forløb - Sommeren 2023 - Runde 31.json",
     "KBU Forløb - Vinter 2023_2024 - Runde 32.json",
 ]
-
-EVALUERING_FILE = "evaluering.json"
 
 
 class EnhedAfdeling:
@@ -51,95 +48,6 @@ def unikke_enhed_afdelinger():
                 afdeling = e["afdeling"]
                 enhed_afdelinger.add(EnhedAfdeling(enhed, afdeling))
     return list(enhed_afdelinger)
-
-
-def alle_evaluering_sygehuse():
-    url = "https://stamdatakatalog-api-prod.azurewebsites.net/api/Evalueringsstatistik/sygehusForSelect/"
-
-    payload = json.dumps(
-        {
-            "regionIds": [
-                "09f81f66-5166-44fb-a6c7-2bc6d551f1ca",
-                "7b004954-ff3e-4469-ab3f-bad145a2ee88",
-                "8bcf060f-f28f-405f-ab23-fb51391ca69c",
-                "00fc2b0f-9a66-44d9-b45f-c6b638c39cea",
-                "7e28dde8-05a1-4ed8-ac8c-90f5d26719f0",
-                "4433acdd-e5d9-4588-ba2c-9cda1cea811f",
-                "5c799b4a-8a0a-4bea-926e-7dd62393d396",
-            ],
-            "includeInactive": False,
-        }
-    )
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0",
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Referer": "https://uddannelseslaege.dk/",
-        "content-type": "application/json",
-        "Origin": "https://uddannelseslaege.dk",
-        "Connection": "keep-alive",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "Priority": "u=4",
-        "Cookie": "ARRAffinity=6baab673920cdd72d5292ed70f53157373a58e78fa4d23722efa3ae6748c9e7b; ARRAffinitySameSite=6baab673920cdd72d5292ed70f53157373a58e78fa4d23722efa3ae6748c9e7b",
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    # {
-    # "failed": false,
-    # "data": [
-    #     {
-    #         "id": "1c1d0000-0000-0000-0000-000000000000",
-    #         "text": "Almen praksis område Vest - almen praksis uspecificeret",
-    #         "groupText": null,
-    #         "subText": null
-    #     },
-    # ...
-    return json.loads(response.text)["data"]
-
-
-def alle_evaluering_specialer():
-    url = "https://stamdatakatalog-api-prod.azurewebsites.net/api/Evalueringsstatistik/specialerForSelect"
-
-    payload = {}
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0",
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Referer": "https://uddannelseslaege.dk/",
-        "Origin": "https://uddannelseslaege.dk",
-        "Connection": "keep-alive",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "Priority": "u=4",
-        "Cookie": "ARRAffinity=6baab673920cdd72d5292ed70f53157373a58e78fa4d23722efa3ae6748c9e7b; ARRAffinitySameSite=6baab673920cdd72d5292ed70f53157373a58e78fa4d23722efa3ae6748c9e7b",
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    # {
-    # "failed": false,
-    # "data": [
-    #     {
-    #         "id": "e21fd32b-eb14-4069-acbc-352916bbb708",
-    #         "text": "Akutmedicin",
-    #         "groupText": null,
-    #         "subText": null
-    #     },
-    # ...
-    return json.loads(response.text)["data"]
-
-
-def extractText(x):
-    if isinstance(x, dict):
-        return x["text"]
-    else:
-        return x
 
 
 def alle_afdelinger():
@@ -183,69 +91,6 @@ def alle_afdelinger():
     response = requests.request("POST", url, headers=headers, data=payload)
 
     return json.loads(response.text)["data"]
-
-
-def afdelinger_fra_sygehus(sygehusId):
-    import requests
-    import json
-
-    url = "https://stamdatakatalog-api-prod.azurewebsites.net/api/Evalueringsstatistik/afdelinger/"
-
-    payload = json.dumps(
-        {
-            "regionIds": [
-                "09f81f66-5166-44fb-a6c7-2bc6d551f1ca",
-                "7b004954-ff3e-4469-ab3f-bad145a2ee88",
-                "8bcf060f-f28f-405f-ab23-fb51391ca69c",
-                "00fc2b0f-9a66-44d9-b45f-c6b638c39cea",
-                "7e28dde8-05a1-4ed8-ac8c-90f5d26719f0",
-                "4433acdd-e5d9-4588-ba2c-9cda1cea811f",
-                "5c799b4a-8a0a-4bea-926e-7dd62393d396",
-            ],
-            "sygehusIds": [sygehusId],
-            "includeInactive": False,
-        }
-    )
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0",
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Referer": "https://uddannelseslaege.dk/",
-        "content-type": "application/json",
-        "Origin": "https://uddannelseslaege.dk",
-        "Connection": "keep-alive",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-        "Priority": "u=4",
-        "Cookie": "ARRAffinity=6c5458dd548284d7396cd2cba264e633b6118968d745d1e82e3c3f7fc14794d7; ARRAffinitySameSite=6c5458dd548284d7396cd2cba264e633b6118968d745d1e82e3c3f7fc14794d7",
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    # {
-    # "failed": false,
-    # "data": [
-    #     {
-    #         "id": "0e6aafad-cb29-807c-dba2-7cca59f2dbba",
-    #         "text": "662030 (Aarhus Universitetshospital)",
-    #         "groupText": null,
-    #         "subText": null
-    #     },
-    # ...
-    return json.loads(response.text)["data"]
-
-
-def unikke_enheder():
-    enheder = set()
-    for f in FILES:
-        with open(f) as file:
-            elements = json.load(file)
-            for e in elements:
-                enhed = e["enhed"]
-                enheder.add(enhed)
-    return enheder
 
 
 def kbu_evalueringer_for_uddannelsessteder(uddannelsesstedIds):
@@ -360,31 +205,6 @@ def kbu_evalueringer_for_uddannelsessteder(uddannelsesstedIds):
     return json.loads(response.text)["data"]
 
 
-def fuzzy_select(query, processExtracts):
-    if len(processExtracts) == 0:
-        # If not matches return none
-        print("Unknown")
-        return None
-    # elif processExtracts[0][1] == 100:
-    #     # If 100 % match return that
-    #     return processExtracts[0][0]
-    else:
-        print(query, "?")
-        # List options and take user input
-        i = 0
-        for f in processExtracts:
-            print(f"\t{i}: {f}")
-            i += 1
-
-        choice = input("> ")
-        if choice == "break" or choice == "none":
-            return None
-        elif not choice:
-            choice = 0
-
-        return processExtracts[int(choice)][0]
-
-
 # Målet er at finde uddannelsesstedId for hvert enhed:afdeling par, der optræder i KBU forløb filerne
 # Løgn: Sygehus navnene er et 1-til-1 match med "enhed" navnene (i hvert fald case-insensitive)
 # Uddannelsesstednavn fra "afdeling" navn skal være manuelt verificeret (med fuzzy matching som hjælp)
@@ -406,87 +226,57 @@ else:
 
 enhed_afdelinger = unikke_enhed_afdelinger()
 
-alle_sygehuse = alle_evaluering_sygehuse()
-
-enhed_sygehuse = {}
-if os.path.exists("enhed_sygehuse.json"):
-    with open("enhed_sygehuse.json") as temp:
-        enhed_sygehuse = json.load(temp)
-else:
-    for e in unikke_enheder():
-        fuzzySygehus = process.extract(e, set([x["text"] for x in alle_sygehuse]))
-        match = fuzzy_select(e, fuzzySygehus)
-
-        chosen_sygehus = [x for x in alle_sygehuse if x["text"] == match][0]
-        enhed_sygehus = {
-            "sygehus": chosen_sygehus["text"],
-            "sygehusId": chosen_sygehus["id"],
-        }
-        print(e, enhed_sygehus)
-        enhed_sygehuse[e] = enhed_sygehus
-
-    with open("enhed_sygehuse.json", "w") as temp:
-        print(enhed_sygehuse)
-        json.dump(enhed_sygehuse, temp, ensure_ascii=False)
-
-afdeling_uddannelsessteder = []
-for ea in enhed_afdelinger[1:100]:
-    fuzzySygehus = process.extract(
-        ea.enhed, set([x["sygehusNavn"] for x in alle_kbu_evalueringer])
-    )
-    sygehus = fuzzy_select(ea.enhed, fuzzySygehus)
-
-    fuzzyUddannelsessted = process.extract(
-        ea.afdeling,
-        set([
-            x["uddannelsesstedNavn"]
+evalueringer = []
+for ea in random.choices(enhed_afdelinger, k=5):
+    possibilities = []
+    for sygehus in set([x["sygehusNavn"] for x in alle_kbu_evalueringer]):
+        for uddannelsessted in set(
+            [
+                x["uddannelsesstedNavn"]
+                for x in alle_kbu_evalueringer
+                if x["sygehusNavn"] == sygehus
+            ]
+        ):
+            possibilities.append(
+                (
+                    uddannelsessted,
+                    sygehus,
+                    (
+                        fuzz.WRatio(ea.enhed, sygehus)
+                        + fuzz.WRatio(ea.afdeling, uddannelsessted)
+                    )
+                    / 2,
+                )
+            )
+    sortedPossibilities = sorted(possibilities, key=lambda x: x[2], reverse=True)
+    i = 0
+    sortedPossibilities.reverse()
+    for sp in sortedPossibilities:
+        print(len(sortedPossibilities) - i - 1, sp[0], sp[1])
+        i += 1
+    print()
+    print(ea.afdeling, ea.enhed)
+    choice = input("> ")
+    if not choice:
+        print("Ingen evalueringer fundet for", ea.afdeling, ea.enhed)
+        match = None
+    else:
+        temp = sortedPossibilities[len(sortedPossibilities) - int(choice) - 1]
+        match = next(
+            x
             for x in alle_kbu_evalueringer
-            if x["sygehusNavn"] == sygehus
-        ]),
-        limit=100,
-    )
-    match = fuzzy_select(f"{ea.afdeling} ({sygehus})", fuzzyUddannelsessted)
+            if x["uddannelsesstedNavn"] == temp[0] and x["sygehusNavn"] == temp[1]
+        )
 
-    # if len(fuzzyUddannelsessted) == 0:
-    #     print("Unknown", ea)
-    #     continue
-    # elif fuzzyUddannelsessted[0][1] == 100:
-    #     match = fuzzyUddannelsessted[0][0]
-    # else:
-    #     print(ea.afdeling, "?")
-    #     i = 0
-    #     for f in fuzzyUddannelsessted:
-    #         print(f"\t{i}: {f}")
-    #         i += 1
-    #     choice = input("> ")
-    #     if choice == "break":
-    #         afdeling_uddannelsessted = {
-    #             "enhed": ea.enhed,
-    #             "sygehus": enhed_sygehuse[ea.enhed],
-    #             "afdeling": ea.afdeling,
-    #         }
-    #         print(afdeling_uddannelsessted)
-    #         print()
-    #         afdeling_uddannelsessteder.append(afdeling_uddannelsessted)
-    #         break
-    #     elif not choice:
-    #         choice = 0
-    #     match = fuzzyUddannelsessted[int(choice)][0]
-    chosen_uddannelsessted = next(
-        (x for x in alle_kbu_evalueringer if x["uddannelsesstedNavn"] == match),
-        {"uddannelsesstedNavn": None, "uddannelsesstedId": None},
-    )
     afdeling_uddannelsessted = {
         "enhed": ea.enhed,
-        "sygehus": enhed_sygehuse[ea.enhed],
         "afdeling": ea.afdeling,
-        "uddannelsesstedNavn": chosen_uddannelsessted["uddannelsesstedNavn"],
-        "uddannelsesstedId": chosen_uddannelsessted["uddannelsesstedId"],
+        "evaluering": match,
     }
 
     print(afdeling_uddannelsessted)
     print()
-    afdeling_uddannelsessteder.append(afdeling_uddannelsessted)
+    evalueringer.append(afdeling_uddannelsessted)
 
-with open("afdeling_uddannelsessteder.json", "w") as temp:
-    json.dump(afdeling_uddannelsessteder, temp, ensure_ascii=False)
+with open("evalueringer.json", "w") as temp:
+    json.dump(evalueringer, temp, ensure_ascii=False)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import json
 
+# Exported from Google Sheets as .tsv
 FILES = [
     "KBU Forløb - Sommeren 2022 - Runde 29.tsv",
     "KBU Forløb - Vinter 2022_2023 - Runde 30.tsv",
@@ -8,32 +9,14 @@ FILES = [
     "KBU Forløb - Vinter 2023_2024 - Runde 32.tsv",
 ]
 
-FORMAT = "JSON"
-# FORMAT = "CSV"
-
-if (FORMAT == "CSV"):
+for filename in FILES:
     results = []
-    for filename in FILES:
-        with open(filename) as file:
-            reader = csv.reader(file, delimiter='\t')
-            next(reader)
-            for line in reader:
-                results.append([line[0], line[2], line[3], line[4]])
+    with open(filename) as file:
+        reader = csv.reader(file, delimiter='\t')
+        next(reader)
+        for line in reader:
+            results.append({"nummer": int(line[0]), "speciale": line[2], "enhed": line[3], "afdeling": line[4]})
 
-    with open("result.csv", "w") as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(["Nummer", "Speciale", "Enhed", "Afdeling"])
-        for r in results:
-            writer.writerow(r)
-else: #JSON
-    for filename in FILES:
-        results = []
-        with open(filename) as file:
-            reader = csv.reader(file, delimiter='\t')
-            next(reader)
-            for line in reader:
-                results.append({"nummer": int(line[0]), "speciale": line[2], "enhed": line[3], "afdeling": line[4]})
-
-        sortedResults = sorted(results, key=lambda x:int(x["nummer"]))
-        with open(filename.replace("tsv", "json"), "w") as file:
-            json.dump(sortedResults, file, ensure_ascii=False)
+    sortedResults = sorted(results, key=lambda x:int(x["nummer"]))
+    with open(filename.replace("tsv", "json"), "w") as file:
+        json.dump(sortedResults, file, ensure_ascii=False)
